c6062b326557de906d129ff8deceeecf9c1f1237
Juho Makkonen <juho.makkonen@tkk.fi>
2008-06-18 06:39:28 +0000

    Connection-model

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -8,12 +8,12 @@ class PersonTest < ActiveSupport::TestCase
     @valid_person = people(:valid_person) 
     @invalid_person = people(:invalid_person) 
   end
-  
+
   # This person should be valid by construction. 
   def test_person_validity 
     assert people(:valid_person).valid? 
   end
-  
+
   # This person should be invalid by construction. 
   def test_person_invalidity 
     assert !@invalid_person.valid? 
@@ -22,101 +22,101 @@ class PersonTest < ActiveSupport::TestCase
       assert @invalid_person.errors.invalid?(attribute) 
     end 
   end
-  
+
   # Check uniqueness of username.
   def test_uniqueness_of_username
     person_repeat = Person.new(:username => @valid_person.username)
     assert !person_repeat.valid?
     assert_equal @error_messages[:taken], person_repeat.errors.on(:username)
   end
-  
+
   # Make sure the screen name can't be too short. 
-    def test_username_minimum_length 
-      person = @valid_person 
-      min_length = Person::USERNAME_MIN_LENGTH 
+  def test_username_minimum_length 
+    person = @valid_person 
+    min_length = Person::USERNAME_MIN_LENGTH 
 
-      # Screen name is too short. 
-      person.username = "a" * (min_length - 1) 
-      assert !person.valid?, "#{person.username} should raise a minimum length error" 
-      # Format the error message based on minimum length. 
-      correct_error_message = sprintf(@error_messages[:too_short], min_length) 
-      assert_equal correct_error_message, person.errors.on(:username) 
+    # Screen name is too short. 
+    person.username = "a" * (min_length - 1) 
+    assert !person.valid?, "#{person.username} should raise a minimum length error" 
+    # Format the error message based on minimum length. 
+    correct_error_message = sprintf(@error_messages[:too_short], min_length) 
+    assert_equal correct_error_message, person.errors.on(:username) 
 
-      # Screen name is minimum length. 
-      person.username = "a" * min_length 
-      assert person.valid?, "#{person.username} should be just long enough to pass" 
-    end
+    # Screen name is minimum length. 
+    person.username = "a" * min_length 
+    assert person.valid?, "#{person.username} should be just long enough to pass" 
+  end
 
-    # Make sure the screen name can't be too long. 
-    def test_username_maximum_length 
-      person = @valid_person 
-      max_length = Person::USERNAME_MAX_LENGTH 
+  # Make sure the screen name can't be too long. 
+  def test_username_maximum_length 
+    person = @valid_person 
+    max_length = Person::USERNAME_MAX_LENGTH 
 
-      # Screen name is too long. 
-      person.username = "a" * (max_length + 1) 
-      assert !person.valid?, "#{person.username} should raise a maximum length error" 
+    # Screen name is too long. 
+    person.username = "a" * (max_length + 1) 
+    assert !person.valid?, "#{person.username} should raise a maximum length error" 
 
-      # Format the error message based on maximum length 
-      correct_error_message = sprintf(@error_messages[:too_long], max_length) 
-      assert_equal correct_error_message, person.errors.on(:username) 
+    # Format the error message based on maximum length 
+    correct_error_message = sprintf(@error_messages[:too_long], max_length) 
+    assert_equal correct_error_message, person.errors.on(:username) 
 
-      # Screen name is maximum length. 
-      person.username = "a" * max_length 
-      assert person.valid?, "#{person.username} should be just short enough to pass" 
-    end
+    # Screen name is maximum length. 
+    person.username = "a" * max_length 
+    assert person.valid?, "#{person.username} should be just short enough to pass" 
+  end
 
-    # Make sure the password can't be too short. 
-    def test_password_minimum_length 
-      person = @valid_person 
-      min_length = Person::PASSWORD_MIN_LENGTH 
+  # Make sure the password can't be too short. 
+  def test_password_minimum_length 
+    person = @valid_person 
+    min_length = Person::PASSWORD_MIN_LENGTH 
 
-      # Password is too short. 
-      person.password = "a" * (min_length - 1) 
-      assert !person.valid?, "#{person.password} should raise a minimum length error" 
-      # Format the error message based on minimum length. 
-      correct_error_message = sprintf(@error_messages[:too_short], min_length) 
-      assert_equal correct_error_message, person.errors.on(:password) 
+    # Password is too short. 
+    person.password = "a" * (min_length - 1) 
+    assert !person.valid?, "#{person.password} should raise a minimum length error" 
+    # Format the error message based on minimum length. 
+    correct_error_message = sprintf(@error_messages[:too_short], min_length) 
+    assert_equal correct_error_message, person.errors.on(:password) 
 
-      # Password is just long enough. 
-      person.password = "a" * min_length 
-      assert person.valid?, "#{person.password} should be just long enough to pass" 
+    # Password is just long enough. 
+    person.password = "a" * min_length 
+    assert person.valid?, "#{person.password} should be just long enough to pass" 
+  end 
+
+  # Make sure the password can't be too long. 
+  def test_password_maximum_length 
+    person = @valid_person 
+    max_length = Person::PASSWORD_MAX_LENGTH 
+
+    # Password is too long. 
+    person.password = "a" * (max_length + 1) 
+    assert !person.valid?, "#{person.password} should raise a maximum length error" 
+    # Format the error message based on maximum length. 
+    correct_error_message = sprintf(@error_messages[:too_long], max_length) 
+    assert_equal correct_error_message, person.errors.on(:password) 
+
+    # Password is maximum length. 
+    person.password = "a" * max_length 
+    assert person.valid?, "#{person.password} should be just short enough to pass" 
+  end
+
+  # Test the validations involving screen name with valid examples.
+  def test_username_with_valid_examples 
+    person = @valid_person 
+    valid_usernames = %w{juho kusti juho_kusti} 
+    valid_usernames.each do |username| 
+      person.username = username 
+      assert person.valid?, "#{username} should pass validation, but doesn't" 
     end 
+  end 
 
-    # Make sure the password can't be too long. 
-    def test_password_maximum_length 
-      person = @valid_person 
-      max_length = Person::PASSWORD_MAX_LENGTH 
-
-      # Password is too long. 
-      person.password = "a" * (max_length + 1) 
-      assert !person.valid?, "#{person.password} should raise a maximum length error" 
-      # Format the error message based on maximum length. 
-      correct_error_message = sprintf(@error_messages[:too_long], max_length) 
-      assert_equal correct_error_message, person.errors.on(:password) 
-
-      # Password is maximum length. 
-      person.password = "a" * max_length 
-      assert person.valid?, "#{person.password} should be just short enough to pass" 
-    end
-
-    # Test the validations involving screen name with valid examples.
-    def test_username_with_valid_examples 
-      person = @valid_person 
-      valid_usernames = %w{juho kusti juho_kusti} 
-      valid_usernames.each do |username| 
-        person.username = username 
-        assert person.valid?, "#{username} should pass validation, but doesn't" 
-      end 
+  # Test the validations involving screen name with invalid examples.
+  def test_username_with_invalid_examples 
+    person = @valid_person 
+    invalid_usernames = %w{rails/rocks web2.0 javscript:something} 
+    invalid_usernames.each do |username| 
+      person.username = username
+      assert !person.valid?, "#{name} shouldn't pass validation, but does" 
     end 
+  end
 
-    # Test the validations involving screen name with invalid examples.
-    def test_username_with_invalid_examples 
-      person = @valid_person 
-      invalid_usernames = %w{rails/rocks web2.0 javscript:something} 
-      invalid_usernames.each do |username| 
-        person.username = username
-        assert !person.valid?, "#{name} shouldn't pass validation, but does" 
-      end 
-    end
-    
 end
