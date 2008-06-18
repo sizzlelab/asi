735ba198b28b466d308054ffc76066339e3e779d
Juho Makkonen <juho.makkonen@tkk.fi>
2008-06-18 10:20:42 +0000

    Connection-modelin viimeistelty versio & testit.

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,7 +1,49 @@
 class Connection < ActiveRecord::Base
   belongs_to :person
-  # Refer the second person as "connection of person".
+  # Refer to the second person as "contact".
   belongs_to :contact, :class_name => "Person", :foreign_key => "contact_id"
-  
   validates_presence_of :person_id, :contact_id
+  
+  # Return true if the people are (possibly pending) contacts.
+  def self.exists?(person, contact)
+    not find_by_person_id_and_contact_id(person, contact).nil?
+  end
+  
+  # Record a pending connection request.
+  def self.request(person, contact)
+    unless person == contact or Connection.exists?(person, contact)
+      transaction do
+        create(:person => person, :contact => contact, :status => 'pending')
+        create(:person => contact, :contact => person, :status => 'requested')
+      end
+    end
+  end
+  
+  # Accept a connection request.
+  def self.accept(person, contact)
+    transaction do
+      updated_at = Time.now
+      accept_one_side(person, contact, updated_at)
+      accept_one_side(contact, person, updated_at)
+    end
+  end
+  
+  # Delete a connection or cancel a pending request.
+  def self.breakup(person, contact)
+    transaction do
+      destroy(find_by_person_id_and_contact_id(person, contact))
+      destroy(find_by_person_id_and_contact_id(contact, person))
+    end
+  end
+  
+  private
+  
+  # Update the db with one side of an accepted connection request.
+  def self.accept_one_side(person, contact, updated_at)
+    request = find_by_person_id_and_contact_id(person, contact)
+    request.status = 'accepted'
+    request.updated_at = updated_at
+    request.save!
+  end
+  
 end
--- test/unit/connection_test.rb
+++ test/unit/connection_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class ConnectionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def setup
+    @person = people(:valid_person)
+    @contact = people(:contact)
   end
+
+  def test_request
+    Connection.request(@person, @contact)
+    assert Connection.exists?(@person, @contact)
+    assert_status @person, @contact, 'pending'
+    assert_status @contact, @person, 'requested'
+  end
+  
+  def test_accept
+    Connection.request(@person, @contact)
+    Connection.accept(@person, @contact)
+    assert Connection.exists?(@person, @contact)
+    assert_status @person, @contact, 'accepted'
+    assert_status @contact, @person, 'accepted'  
+  end
+
+  def test_breakup
+    Connection.request(@person, @contact)
+    Connection.breakup(@person, @contact)
+    assert !Connection.exists?(@person, @contact)
+  end
+
+  private
+
+  # Verify the existence of a connection with the given status.
+  def assert_status(person, contact, status)
+    connection = Connection.find_by_person_id_and_contact_id(person, contact)
+    assert_equal status, connection.status
+  end
+
 end
