309fa3f7d6d386d4b87daaf5a4a402e53ef550af
Ville Sundberg <sundberg.ville@gmail.com>
2008-06-24 07:15:57 +0000

    #15 Collectineiden kayttajakohtainen kontrolli

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,13 +1,33 @@
 class Collection < ActiveRecord::Base
   usesguid
+
   has_many_polymorphs :items, :from => [:text_items, :binary_items], :through => :ownerships
   belongs_to :owner, :class_name => "Person"
   belongs_to :client
 
   def to_json(*a)
     {
-      'entry'         => items
+      'id' => id,
+      'entry' => items
     }.to_json(*a)
+  end
+
+  # Returns true if the given person, using the given client, is authorized to view this collection.
+  def read?(person, client)
+    if owner == nil
+      return self.client == client
+    end
+    return (self.client == nil || self.client == client) && (owner == person || owner.contacts.include?(person))
+  end
+
+  # Returns true if the given person, using the given client, is authorized to change this collection.
+  def write?(person, client)
+    return read?(person, client) && (owner == person || ! read_only)
+  end
+
+  # Returns true if the given person, using the given client, is authorized to delete this collection.
+  def delete?(person, client)
+    return write?(person, client) && (owner == person)
   end
 
 end
--- test/functional/collections_controller_test.rb
+++ test/functional/collections_controller_test.rb
@@ -21,26 +21,50 @@ class CollectionsControllerTest < ActionController::TestCase
 
   def test_show
     # Should show a collection belonging to the client
-    get :show, { :app_id => clients(:one).id, :id => collections(:one).id, :format => 'json' }, { :client => clients(:one).id }
+    get :show, { :app_id => clients(:one).id, :id => collections(:one).id, :format => 'json' }, 
+               { :client => clients(:one).id }
 
     assert_response :success
     assert_not_nil assigns["collection"]
 
     # Should not show a collection belonging to another client
-    get :show, { :app_id => clients(:two).id, :id => collections(:one).id, :format => 'json' }, { :client => clients(:two).id }
+    get :show, { :app_id => clients(:two).id, :id => collections(:one).id, :format => 'json' }, 
+               { :client => clients(:two).id }
 
     assert_response :forbidden
     assert_nil assigns["collection"]
 
     # Should not show a collection belonging to another user
-    get :show, { :id => collections(:one).id, :format => 'json' }, { :client => clients(:two).id, :user => people(:contact) }
+    get :show, { :id => collections(:one).id, :format => 'json' }, 
+               { :client => clients(:two).id, :user => people(:contact) }
 
     assert_response :forbidden
     assert_nil assigns["collection"]
+
+    # Should show a collection belonging to a connection of the user
+    get :show, { :app_id => clients(:one).id, :id => collections(:three).id, :format => 'json' }, 
+               { :client => collections(:three).client.id, :user => people(:valid_person).id }
+
+    assert people(:valid_person).contacts.include?(collections(:three).owner)
+    assert_response :success
+    assert_not_nil assigns["collection"]
+
+    # Should not show a collection beloning to a requested connection
+    get :show, { :app_id => clients(:one).id, :id => collections(:four).id, :format => 'json' }, 
+               { :client => clients(:one).id, :user => people(:valid_person) }
+
+    assert_response :forbidden
+    assert_nil assigns["collection"]
+
+    # Should not crash on a nonexistent collection
+    get :show, { :app_id => clients(:one).id, :id => 2340982349078, :format => 'json' }, 
+               { :client => clients(:one).id }
+    assert_response 404
+    assert_nil assigns["collection"]
+
   end
 
   def test_create
-
     # With an owner and a client
     post :create, { :app_id => clients(:one).id, :format => 'json', :owner => people(:valid_person).id }, 
                   { :user => people(:valid_person).id, :client => clients(:one).id }
@@ -50,14 +74,16 @@ class CollectionsControllerTest < ActionController::TestCase
     assert_equal(assigns["collection"].client, clients(:one))
 
     # With only a client
-    post :create, { :app_id => clients(:one).id, :format => 'json'}, { :client => clients(:one).id }
+    post :create, { :app_id => clients(:one).id, :format => 'json'}, 
+                  { :client => clients(:one).id }
     assert_response :success
     assert_not_nil assigns["collection"]
     assert_nil assigns["collection"].owner
     assert_equal(assigns["collection"].client, clients(:one))
 
     # With only an owner
-    post :create, { :format => 'json' }, { :user => people(:valid_person).id }
+    post :create, { :format => 'json' }, 
+                  { :user => people(:valid_person).id }
     assert_response :forbidden
 
     # With neither
@@ -71,21 +97,42 @@ class CollectionsControllerTest < ActionController::TestCase
                     { :client => clients(:one).id }
     assert_response :success
 
+    get :show, { :app_id => clients(:one).id, :id => collections(:one).id, :format => 'json' }, 
+               { :client => clients(:one).id }
+    assert_response :not_found
+
     # Should not allow POST
-    post :delete, { :app_id => clients(:one).id, :id => collections(:two).id, :format => 'json' }, { :client => clients(:one).id }
+    post :delete, { :app_id => clients(:one).id, :id => collections(:two).id, :format => 'json' }, 
+                  { :client => clients(:one).id }
     assert_response :method_not_allowed
 
-    # Should allow POST overridden with DELETE
-    @request.set_header "X-HTTP-Method-Override", "DELETE"
-    post :delete, { :app_id => clients(:one).id, :id => collections(:two).id, :format => 'json' }, { :client => clients(:one).id }
+    # TODO: Should allow POST overridden with DELETE
+#     @request.set_header "X-HTTP-Method-Override", "DELETE"
+#     post :delete, { :app_id => clients(:one).id, :id => collections(:two).id, :format => 'json' }, 
+#                   { :client => clients(:one).id }
+#     assert_response :success
+    
+#     get :show, { :app_id => clients(:one).id, :id => collections(:two).id, :format => 'json' }, 
+#                { :client => clients(:one).id }
+#     assert_response :not_found
 
     # Should not delete a collection belonging to another client
-    delete :delete, { :id => collections(:two).id, :format => 'json' }, { :client => clients(:one).id }
+    delete :delete, { :id => collections(:two).id, :format => 'json' }, 
+                    { :client => clients(:one).id }
     assert_response :forbidden
 
     # Should not delete a collection belonging to another user
-    delete :delete, { :id => collections(:two).id, :user => people(:valid_person).id, :format => 'json' }, { :client => clients(:two).id }
+    delete :delete, { :id => collections(:two).id, :user => people(:valid_person).id, :format => 'json' }, 
+                    { :client => clients(:two).id }
     assert_response :forbidden
+
+    # Should not delete a collection belonging to a friend
+    delete :delete, { :app_id => clients(:one).id, :id => collections(:three).id, :format => 'json' }, 
+                    { :client => collections(:three).client.id, :user => people(:valid_person).id }
+
+    assert people(:valid_person).contacts.include?(collections(:three).owner)
+    assert_response :forbidden
+
   end
 
 end
