2f4ef52f63095429242195d721815ad97ad0765c
Ville Sundberg <sundberg.ville@gmail.com>
2008-06-27 10:48:19 +0000

    buildskriptäi

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ rm -rf /usr/local/common-services
 svn export file:///svn/common-services/trunk /usr/local/common-services
 cd /usr/local/common-services
 script/server -d -e production
+sudo /etc/init.d/httpd restart
+
