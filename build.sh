6cfeadafdca674a783593be74ea1a73fc7c9578b
Ville Sundberg <sundberg.ville@gmail.com>
2008-06-27 08:20:03 +0000

    Tuontantoserveriskripti

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /
+rm -rf /usr/local/common-services
+svn export file:///svn/common-services/trunk /usr/local/common-services
+cd /usr/local/common-services
+script/server -d -e production
