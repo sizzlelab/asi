d965e0af10df1402675b6f21d5c4ac2598f2ad2f
Ville Sundberg <sundberg.ville@gmail.com>
2008-06-30 12:29:06 +0000

    #27 ja buildskriptiä

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,6 +5,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  layout 'default'
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
