e5bb4e7aba9b2fb7b3ebf40385a08cae14444f90
Ville Sundberg <sundberg.ville@gmail.com>
2008-06-30 12:41:14 +0000

    CSS-säätöä ja buildiskriptiä

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # A crude Common Services build script specific to the maps.cs.hut.fi machine.
+
+killall mongrel_rails
 cd /
 rm -rf /usr/local/common-services
 svn export file:///svn/common-services/trunk /usr/local/common-services
