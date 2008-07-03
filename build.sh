cd238bcd95e74a98c330606f2173fdc604bfb8db
Ville Sundberg <sundberg.ville@gmail.com>
2008-07-03 13:02:18 +0000

    Buildskripti jaettu

--- build.sh
+++ build.sh
@@ -6,11 +6,4 @@ cd /
 rm -rf /usr/local/common-services
 svn export file:///svn/common-services/trunk /usr/local/common-services
 cd /usr/local/common-services
-REV=$((`svn info file:///svn/common-services | grep "^Last Changed Rev" | perl -pi -e "s/Last Changed Rev: //"`-`svn info file:///svn/common-services/tags | grep "^Last Changed \
-Rev" | perl -pi -e "s/Last Changed Rev: //"`))
-echo $REV > app/views/layouts/_revision.html.erb
-echo "http://maps.cs.hut.fi/cos/" > app/views/layouts/_servername.html.erb
-date > app/views/layouts/_build_date.html.erb
-script/server -d -e production
-sudo /etc/init.d/httpd restart
-
+./finish.sh
