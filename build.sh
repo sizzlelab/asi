94ee0409f4689117af085cccdca9140612a7abd4
Ville Sundberg <sundberg.ville@gmail.com>
2008-07-03 12:58:39 +0000

    Buildskriptistä bugi pois

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ REV=$((`svn info file:///svn/common-services | grep "^Last Changed Rev" | perl -
 Rev" | perl -pi -e "s/Last Changed Rev: //"`))
 echo $REV > app/views/layouts/_revision.html.erb
 echo "http://maps.cs.hut.fi/cos/" > app/views/layouts/_servername.html.erb
-echo date > app/views/layouts/_build_date.html.erb
+date > app/views/layouts/_build_date.html.erb
 script/server -d -e production
 sudo /etc/init.d/httpd restart
 
