7f4ff2a3f3cc2dae59e10158a1c01de733218d9f
Ville Sundberg <sundberg.ville@gmail.com>
2008-07-13 16:42:39 +0000

    Reitteja DRY:tetty

--- config/routes.rb
+++ config/routes.rb
@@ -1,135 +1,97 @@
+module COSRoutes
+  def resource(route, options)
+    options.except(:controller).each do |method, action|
+      connect route, :controller => options[:controller],
+                     :format => 'json',
+                     :action => action,
+                     :conditions => { :method => method }
+    end
+  end
+end
+
+
 ActionController::Routing::Routes.draw do |map|
+
+  map.extend(COSRoutes)
 
   map.connect '/appdata/user_id/@self/app_id', :controller => 'client_data',
                                                :action => 'show',
                                                :format => 'html',
                                                :conditions => { :method => :get }
 
-  map.connect '/appdata/:user_id/@self/:app_id', :controller => 'client_data',
-                                                 :action => 'show',
-                                                 :format => 'json',
-                                                 :conditions => { :method => :get}
-
-  map.connect '/appdata/:user_id/@self/:app_id', :controller => 'client_data',
-                                                 :action => 'update',
-                                                 :format => 'json',
-                                                 :conditions => { :method => :put}
+  map.resource '/appdata/:user_id/@self/:app_id', :controller => 'client_data',
+                                                  :get => 'show', 
+                                                  :put => 'update'
 
   map.connect '/appdata/app_id/@collections', :controller => 'collections',
                                               :action => 'index',
                                               :format => 'html',
                                               :conditions => { :method => :get }
 
-  map.connect '/appdata/:app_id/@collections', :controller => 'collections', 
-                                               :action => 'index', 
-                                               :format => 'json', 
-                                               :conditions => { :method => :get }
+  map.resource '/appdata/:app_id/@collections', :controller => 'collections',
+                                                :get => 'index', 
+                                                :post => 'create'
 
-  map.connect '/appdata/:app_id/@collections', :controller => 'collections', 
-                                               :action => 'create', 
-                                               :format => 'json', 
-                                               :conditions => { :method => :post }
 
   map.connect '/appdata/app_id/@collections/id', :controller => 'collections',
                                                  :action => 'show',
                                                  :format => 'html',
                                                  :conditions => { :method => :get }
-
-  map.connect '/appdata/:app_id/@collections/:id', :controller => 'collections',
-                                                   :action => 'show',
-                                                   :format => 'json',
-                                                   :conditions => { :method => :get }
-
-  map.connect '/appdata/:app_id/@collections/:id', :controller => 'collections',
-                                                   :action => 'delete',
-                                                   :format => 'json',
-                                                   :conditions => { :method => :delete }                          
-
-  map.connect '/appdata/:app_id/@collections/:id', :controller => 'collections',
-                                                   :action => 'add',
-                                                   :format => 'json',
-                                                   :conditions => { :method => :post } 
+  
+  map.resource '/appdata/:app_id/@collections/:id', :controller => 'collections',
+                                                    :get => 'show',
+                                                    :delete => 'delete',
+                                                    :post => 'add'
 
   map.connect '/appdata', :controller => 'client_data',
                           :action => 'index',
                           :format => 'html',
                           :conditions => { :method => :get }
 
-  map.connect 'people/user_id/@self', :controller => 'people',
+  map.connect '/people/user_id/@self', :controller => 'people',
                                        :action => 'user_id_@self',                                 
                                        :format => 'html',
                                        :conditions => { :method => :get }
 
-  map.connect '/people/:user_id/@self', :controller => 'people', 
-                                        :action => 'show', 
-                                        :format => 'json', 
-                                        :conditions => { :method => :get }
-                          
-  map.connect '/people/:user_id/@self', :controller => 'people', 
-                                   :action => 'update', 
-                                   :format => 'json', 
-                                   :conditions => { :method => :put }
+  
+  map.resource '/people/:user_id/@self', :controller => 'people',
+                                         :get => 'show', 
+                                         :put => 'update', 
+                                         :delete => 'delete'
 
-  map.connect 'people/:user_id/@self', :controller => 'people',
-                                  :action => 'delete',
-                                  :format => 'json',
-                                  :conditions => { :method => :delete }  
-
-  map.connect '/people',  :controller => 'people',
-                          :action => 'create',
-                          :format => 'json',
-                          :conditions => { :method => :post}
+  map.resource '/people', :controller => 'people',
+                          :post => 'create'
 
   map.connect '/people/user_id/@friends', :controller => 'people',
                                            :action => 'user_id_@friends',
                                            :format => 'html',
                                            :conditions => { :method => :get }                          
-                                                                
-  map.connect '/people/:user_id/@friends', :controller => 'people',
-                                           :action => 'get_friends',
-                                           :format => 'json',
-                                           :conditions => { :method => :get }
-                                            
-  map.connect '/people/:user_id/@friends', :controller => 'people',
-                                           :action => 'add_friend',
-                                           :format => 'json',
-                                           :conditions => { :method => :post }
+
+  map.resource '/people/:user_id/@friends', :controller => 'people',
+                                            :get => 'get_friends',
+                                            :post => 'add_friend'
                                             
   map.connect '/people/:user_id/@friends/:friend_id', :controller => 'people',
                                                       :action => 'user_id_@friends_friend_id',
                                                       :format => 'html',
                                                       :conditions => { :method => :get }
                                                       
-  map.connect '/people/:user_id/@friends/:friend_id', :controller => 'people',
-                                                      :action => 'remove_friend',
-                                                      :format => 'json',
-                                                      :conditions => { :method => :delete }
+  map.resource '/people/:user_id/@friends/:friend_id', :controller => 'people',
+                                                       :delete => 'remove_friend'
+
 
   map.connect '/people/user_id/@location', :controller => 'locations',
                                             :action => 'index',
                                             :format => 'html',
                                             :conditions => { :method => :get }                                                      
 
-  map.connect '/people/:user_id/@location', :controller => 'locations',
-                                            :action => 'get',
-                                            :format => 'json',
-                                            :conditions => { :method => :get }
-                                            
- 
-  map.connect '/people/:user_id/@location', :controller => 'locations',
-                                            :action => 'update',
-                                            :format => 'json',
-                                            :conditions => { :method => :put }
-  
-  map.connect '/session', :controller => 'sessions',
-                          :action => 'destroy',
-                          :format => 'json',
-                          :conditions => { :method => :delete }                                          
-                     
-  map.connect '/session', :controller => 'sessions',
-                          :action => 'create',
-                          :format => 'json',
-                          :conditions => { :method => :post}                                            
+  map.resource '/people/:user_id/@location', :controller => 'locations',
+                                             :get => 'get',
+                                             :put => 'update'
+
+  map.resource '/session', :controller => 'sessions',
+                           :delete => 'destroy',
+                           :post => 'create'
 
   map.connect '/session', :controller => 'sessions',
                           :action => 'index',
