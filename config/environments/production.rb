c2a4d006d380186d8576c0e631a69c573802f349
Antti Virolainen <antti.virolainen@iki.fi>
2008-07-29 09:10:37 +0000

    lisätty relative_url_root production-ympäristöön jotta alpha.sizl.orgin sijaan linkkeihin tulee cos.sizl.org

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,3 +20,6 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+#This should correct the URL:s in links in production site
+ActionController::AbstractRequest.relative_url_root = "http://cos.sizl.org"
