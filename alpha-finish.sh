c860a2e79e5a8429a00bc0b96ec60ff36b8e6a66
Antti Virolainen <antti.virolainen@iki.fi>
2008-09-02 08:41:25 +0000

    Laitettu status messagen timestamp tallentumaan aina UTC aikana.

--- alpha-finish.sh
+++ alpha-finish.sh
@@ -14,4 +14,6 @@ echo $SERVERNAME > app/views/layouts/_servername.html.erb
 date > app/views/layouts/_build_date.html.erb
 script/server -d -e production
 sudo /etc/init.d/apache2 restart
+rake db:migrate
 rake test
+rake db:migrate RAILS_ENV=production
