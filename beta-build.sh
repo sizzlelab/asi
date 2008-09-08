f5d9e785f5b5a42035f3bab36e93d4bd6e7b862c
Antti Virolainen <antti.virolainen@iki.fi>
2008-09-08 13:21:56 +0000

    lisätty build ja finish scriptit myös betaa varten

---
+++ beta-build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# A crude Common Services build script specific to the maps.cs.hut.fi machine.
+# Note that you must run this script twice until changes to this file take effect;
+# changes to finish.sh take effect immediately.
+
+COS_PATH=/var/datat/cos/common-services
+
+killall mongrel_rails
+cd /
+rm -rf $COS_PATH
+svn export --force svn+ssh://alpha.sizl.org/svn/common-services/trunk $COS_PATH
+cd $COS_PATH
+chmod a+x beta-finish.sh
+chgrp -R adm .
+chmod -R 2770 . 
+umask 007
+./beta-finish.sh
