7da2f4f024e5de3e8b0bbe5476cc8c2e35d0a5d9
Antti Virolainen <antti.virolainen@iki.fi>
2008-09-08 13:21:56 +0000

    lisätty build ja finish scriptit myös betaa varten

--- beta-finish.sh
+++ beta-finish.sh
@@ -2,11 +2,11 @@
 # The last part of the Common Services build script. This is in a separate file so that the newest version from the repository
 # is always run. 
 
-SERVERNAME="http://cos.alpha.sizl.org/"
+SERVERNAME="http://cos.sizl.org/"
 
-REV=$((`svn info file:///svn/common-services | \
+REV=$((`svn info svn+ssh://alpha.sizl.org/svn/common-services | \
 grep "^Last Changed Rev" | \
-perl -pi -e "s/Last Changed Rev: //"`-`svn info file:///svn/common-services/tags | \
+perl -pi -e "s/Last Changed Rev: //"`-`svn info svn+ssh://alpha.sizl.org/svn/common-services/tags | \
 grep "^Last Changed Rev" | \
 perl -pi -e "s/Last Changed Rev: //"`))
 echo $REV > app/views/layouts/_revision.html.erb
