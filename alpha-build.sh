1f191baac6b002f4000e958f446876592c2af6f2
Antti Virolainen <antti.virolainen@iki.fi>
2009-01-12 12:21:16 +0000

    virityksiä, joilla COSin pitäisi toimia rails versioilla 2.1 ja 2.2

--- alpha-build.sh
+++ alpha-build.sh
@@ -10,6 +10,8 @@ cd /
 rm -rf $COS_PATH
 svn export --force file:///svn/common-services/trunk $COS_PATH
 cd $COS_PATH
+#the following is to fall back to rails 2.1 compatible version of the plugin
+svn export -r 254 --force file:///svn/common-services/trunk/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb $COS_PATH/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb 
 chmod a+x alpha-finish.sh
 chgrp -R adm .
 chmod -R 2770 . 
