8948500c34caa014dfec03eeff11e50dfd52cc07
Antti Virolainen <antti.virolainen@iki.fi>
2009-03-03 17:38:40 +0000

    fiksattu environmentteja, tuli valitus tupla rails version määrittelystä

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-RAILS_GEM_VERSION = '2.1.0'
-
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
