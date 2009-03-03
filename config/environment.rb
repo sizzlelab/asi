f5eca7f8b3afda4fa07265caa7115a85f889ba04
Antti Virolainen <antti.virolainen@iki.fi>
2009-03-03 17:39:18 +0000

    poistettu kokonaan gem railsin versiomääritys kun aiheutti ongelmia

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,6 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.2.2' unless ENV['RAILS_ENV'] == 'production'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
