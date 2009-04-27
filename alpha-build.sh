6c7cd5c044edbaf50a8bb53faaf99e9a3aaf619b
Antti Virolainen <antti.virolainen@iki.fi>
2009-04-27 16:11:03 +0000

    alpha-buildista rails 2.1 viritys pois

--- alpha-build.sh
+++ alpha-build.sh
@@ -11,8 +11,10 @@ cd /
 rm -rf $COS_PATH
 svn export --force file:///svn/common-services/trunk $COS_PATH
 cd $COS_PATH
+
 #the following is to fall back to rails 2.1 compatible version of the plugin
-svn export -r 254 --force file:///svn/common-services/trunk/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb $COS_PATH/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb 
+#svn export -r 254 --force file:///svn/common-services/trunk/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb $COS_PATH/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb 
+
 mongrel_rails cluster::configure -e production -p 3000 -N 3 -c $COS_PATH -a 127.0.0.1
 chmod a+x alpha-finish.sh
 chgrp -R adm .
