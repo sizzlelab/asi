ea0b4424cbc358c36e70a0625f0b9edae45c6fb0
Antti Virolainen <antti.virolainen@iki.fi>
2009-04-28 12:38:01 +0000

    removed last cludges for rails 2.1 compatibility

--- maps-build.sh
+++ maps-build.sh
@@ -11,7 +11,8 @@ rm -rf $COS_PATH
 svn export svn+ssh://alpha.sizl.org/svn/common-services/trunk $COS_PATH
 cd $COS_PATH
 #the following is to fall back to rails 2.1 compatible version of the plugin
-svn export -r 254 --force svn+ssh://alpha.sizl.org/svn/common-services/trunk/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb $COS_PATH/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb 
+#svn export -r 254 --force svn+ssh://alpha.sizl.org/svn/common-services/trunk/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb $COS_PATH/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb 
+
 chmod a+x maps-finish.sh
 chgrp -R campussourcing .
 chmod -R 2770 . 
