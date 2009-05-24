9e81310eb56860ee0074c69f8226225c761dc065
Antti Virolainen <antti.virolainen@iki.fi>
2009-05-24 19:25:30 +0000

    palaute menee nyt läpi myös nimettömänä

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,5 @@
 class Feedback < ActiveRecord::Base
   belongs_to :author, :class_name => "Person"
   
-  validates_presence_of :content, :author_id, :url
+  validates_presence_of :content, :url
 end
