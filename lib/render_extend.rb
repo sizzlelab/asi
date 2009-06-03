499fcd578fd31cd28b8d912ae134d4c5d0f6bfd2
Sampo Toiva <sampo.toiva@tkk.fi>
2009-06-03 11:00:02 +0000

    #142 Lisatty render monkey patch, joka parsii renderin optioneista :json keyn, tutkii onko se stringi eika JSONin mukainen taulukko. Taman jalkeen lisataan stringeihin [], jotta ne voidaan tulkita JSON:iksi.
    Siis kutsu render :action, :json => "Tama on jsonia".to_json tuottaa nyt validia jsonia.

---
+++ lib/render_extend.rb
@@ -0,0 +1,17 @@
+class ActionController::Base
+  alias_method :old_render, :render
+
+  def render(options = nil, extra_options = {}, &block)
+
+    if options &&
+       options[:json] &&
+       options[:json].class == String &&
+       (options[:json][0].chr != '{' && options[:json][0].chr != '[')
+
+      options[:json] = "[#{options[:json]}]"
+      
+    end
+    
+      old_render options, extra_options, &block
+  end
+end
