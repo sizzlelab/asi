e91c74319d7948d6de12edb9ec6b459af87db27e
Ville Sundberg <sundberg.ville@gmail.com>
2009-06-11 10:09:36 +0000

    #181 luoja automaattisesti jäseneksi ja adminiksi

--- vendor/plugins/simple_groups/lib/simple_groups.rb
+++ vendor/plugins/simple_groups/lib/simple_groups.rb
@@ -16,9 +16,7 @@ module SimpleGroups
     end
 
     module InstanceMethods
-      # methods created for group_plugin
       def is_member_of?(group)
-        #puts self.groups.inspect + "hox"
         self.groups.include?(group)     
       end
 
@@ -30,40 +28,20 @@ module SimpleGroups
         return group.request_membership(self)
       end
 
-      def pending_and_accepted_groups
-        #puts "Kysyttiin acc ja pen GROUPS: ACC: #{self.groups} ja PEN: #{self.pending_groups  } "
-        self.pending_groups + self.groups
-        
-      end
-
-      # use group object to find the membership associated. Use mainly in other methods.
       def membership(group)
-        Membership.find(:first, :conditions => ['person_id = ? AND group_id = ?', self.id, group.id])
+        group.membership(self)
       end
 
       def leave(group)
         self.membership(group).destroy if self.is_member_of?(group)
       end
-      
-      def become_member_of(group)
-        #puts self.pending_and_accepted_groups.inspect + "R1"
-        #self.groups
-        #group.members << self unless self.pending_and_accepted_groups.include?(group)
-        if group.add_member(self) #unless self.pending_and_accepted_groups.include?(group)
-          if group.accept_member(self)
-            group.save
-            return true
-          end
-        end
-        return false
-        #puts self.pending_and_accepted_groups.inspect + "RRR"
-        #puts "#{group.members.inspect} Group members at the moment"
-          # group.members.push self 
-          # puts self.pending_and_accepted_groups.inspect + "RRR222"
-          # puts group.members.inspect + "PAX"
-        
-          
-         # puts "#{group.members.inspect} Group after acceptt"
+
+      def accept_member(person, group)
+        group.accept_member(person) if self.is_admin_of?(group)
+      end
+
+      def invite(person, group)
+        group.invite(person, self)
       end
     end
   end
