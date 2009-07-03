3c3a3c957e11861081513f86e6cae4d03e91eca6
Ville Sundberg <sundberg.ville@gmail.com>
2009-07-03 07:29:07 +0000

    #103 Testejä kantaan meneville ressi-eventeille

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -10,8 +10,15 @@ class ApplicationControllerTest < ActionController::TestCase
   end
 
   def test_index
-    get :index
-    assert_response :success
+    assert_difference "CachedCosEvent.count", 1 do
+      get :index, { :app_id => clients(:one).id }, { :cos_session_id => sessions(:session1).id }
+      assert_response :success
+    end
+    event = CachedCosEvent.find(:first, :order => 'created_at DESC')
+    event.attributes.each do |attribute|
+      assert_not_nil event.send(attribute[0]), attribute[0]
+      assert_not_equal event.send(attribute[0]).to_s.size, 0
+    end
   end
 
 end
