cfb7f29d23d818ec390bd27696602c24031caa2e
Sampo Toiva <sampo.toiva@tkk.fi>
2009-07-09 10:33:42 +0000

    Mass subscription prevention for location token

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,8 @@ class Location < ActiveRecord::Base
   
   belongs_to :person
   
+  attr_protected :location_security_token
+
   validates_numericality_of [:latitude, :longitude, :accuracy], :allow_nil => true
   validates_presence_of [:latitude, :longitude], :unless => :no_lat_long?
 
@@ -9,6 +11,7 @@ class Location < ActiveRecord::Base
      record.errors.add attr, 'is smaller than -180' if value < -180
      record.errors.add attr, 'is greather than 180' if value > 180
   end
+
   validates_each :latitude, :allow_nil  => true do |record, attr, value|
    record.errors.add attr, 'is smaller than -90' if value < -90
    record.errors.add attr, 'is greather than 90' if value > 90
@@ -17,6 +20,14 @@ class Location < ActiveRecord::Base
   # Return true if both latitude and longitude are missing
   def no_lat_long?
     !self.latitude && !self.longitude
+  end
+
+  def get_location_security_token
+    if ! self.location_security_token
+      self.location_security_token = 
+    end
+    
+    return self.location_security_token
   end
 
   def to_json(*a)
