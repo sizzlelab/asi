a4113962304e0b7da4080b760e06a034b56bed8e
Ville Sundberg <sundberg.ville@gmail.com>
2009-07-23 08:29:58 +0000

    Apachen reload pois deploystä

--- alpha-build.sh
+++ alpha-build.sh
@@ -5,17 +5,13 @@
 
 COS_PATH=/var/datat/cos/common-services
 
-#sudo mongrel_rails stop -P $COS_PATH/tmp/pids/mongrel.pid
-sudo mongrel_rails cluster::stop -C $COS_PATH/config/mongrel_cluster.yml
+mongrel_rails cluster::stop -C $COS_PATH/config/mongrel_cluster.yml
 rake thinking_sphinx:stop RAILS_ENV=production
 
 cd /
 rm -rf $COS_PATH
 svn export --force file:///svn/common-services/trunk $COS_PATH
 cd $COS_PATH
-
-#the following is to fall back to rails 2.1 compatible version of the plugin
-#svn export -r 254 --force file:///svn/common-services/trunk/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb $COS_PATH/vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/reflection.rb
 
 mongrel_rails cluster::configure -e production -p 3000 -N 3 -c $COS_PATH -a 127.0.0.1
 chmod a+x alpha-finish.sh
