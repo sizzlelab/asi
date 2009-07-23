7eff0a5fa1c02459170420f1a7c1770708c5950b
Ville Sundberg <sundberg.ville@gmail.com>
2009-07-23 08:29:58 +0000

    Apachen reload pois deploystä

--- alpha-finish.sh
+++ alpha-finish.sh
@@ -12,12 +12,6 @@ ESCAPED_SERVERNAME="http\:\/\/cos\.alpha\.sizl\.org"
 #change COS to use alpha's Ressi
 sed -i "s/localhost\:9000/cos\.alpha\.sizl\.org\/ressi\//" config/environment.rb
 
-# turn on email validation
-#sed -i "s/VALIDATE_EMAILS = false/VALIDATE_EMAILS = true/" config/environment.rb
-
-#change relative url root to SERVERNAME
-#sed -i "s/relative_url_root = \"http\:\/\/cos\.sizl\.org\"/relative_url_root = \"$ESCAPED_SERVERNAME\"/" config/environments/production.rb
-
 #change SERVER_DOMAIN constant to SERVERNAME
 sed -i "s/SERVER_DOMAIN = \"http\:\/\/cos\.sizl\.org\"/SERVER_DOMAIN = \"$ESCAPED_SERVERNAME\"/" config/environments/production.rb
 
@@ -26,15 +20,16 @@ grep "^Last Changed Rev" | \
 perl -pi -e "s/Last Changed Rev: //"`-`svn info file:///svn/common-services/tags | \
 grep "^Last Changed Rev" | \
 perl -pi -e "s/Last Changed Rev: //"`))
+
 echo $REV > app/views/layouts/_revision.html.erb
-#echo $SERVERNAME > app/views/layouts/_servername.html.erb
 date > app/views/layouts/_build_date.html.erb
+
 rake db:migrate RAILS_ENV=production
+
 rake thinking_sphinx:configure RAILS_ENV=production
 rake thinking_sphinx:index RAILS_ENV=production
 rake thinking_sphinx:start RAILS_ENV=production
-#script/server -d -e production
+
 mongrel_rails cluster::start
-sudo /etc/init.d/apache2 restart
 
 crontab config/crontab
