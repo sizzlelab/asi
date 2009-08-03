5b53c43681762221f23fd134fb06104b992bb449
Ville Sundberg <sundberg.ville@gmail.com>
2009-08-03 06:44:32 +0000

    Korjattu Channel tulostamaan käyttäjän guid

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,17 +5,17 @@ class Channel < ActiveRecord::Base
   belongs_to :owner, :class_name => "Person"
   belongs_to :creator_app, :class_name => "Client"
   has_many :messages, :dependent => :destroy
-  has_many :user_subscriptions, :dependent => :destroy 
-  has_many :group_subscriptions, :dependent => :destroy 
+  has_many :user_subscriptions, :dependent => :destroy
+  has_many :group_subscriptions, :dependent => :destroy
   has_many :user_subscribers, :through => :user_subscriptions, :source => :person
   has_many :group_subscribers, :through => :group_subscriptions, :source => :group
 
   attr_accessible :name, :description, :channel_type, :owner, :creator_app
   attr_readonly :channel_type, :creator_app
-  
+
   before_save :subscribe_owner
   before_validation_on_create :set_default_type
-  
+
   validates_inclusion_of :channel_type, :in => %w( public friend group), :message => "must be public, friend or group."
   validates_presence_of :owner_id
   validates_presence_of :creator_app_id
@@ -23,29 +23,29 @@ class Channel < ActiveRecord::Base
   validates_length_of :name, :minimum => 2
   validate :associations_must_exist
   validate :validate_group_count
-  
+
   define_index do
     indexes :name, :sortable => true
     indexes :description
-    
+
     indexes messages(:body), :as => :posts
     indexes messages(:title), :as => :msg_title, :sortable => true
-    
+
     has :guid
     has :created_at
     has :updated_at
-    
+
     set_property :field_wights => { 'name' => 10,
                                     'description' => 5,
                                     'posts' => 2,
                                     'msg_title' => 1 }
-    
+
   end
 
   def can_read?(user)
     if channel_type == "public"
       return true
-    end 
+    end
     if user
       if channel_type == "friend"
         if self.owner == user
@@ -60,25 +60,25 @@ class Channel < ActiveRecord::Base
         end
       end
     end
-    return false 
+    return false
   end
 
   def to_json(*a)
     hash = { :id => guid,
              :name => name,
              :description => description,
-             :owner_id => owner_id,
+             :owner_id => owner.guid,
              :owner_name => owner.name_or_username,
              :created_at => created_at,
              :updated_at => updated_at,
              :channel_type => channel_type,
              :message_count => self.messages.size
-           }    
+           }
     return hash.to_json(*a)
   end
-  
+
   private
-  
+
   def validate_group_count
     if self.channel_type == "group"
       if self.group_subscribers.size > 1
@@ -86,13 +86,13 @@ class Channel < ActiveRecord::Base
       end
     end
   end
-  
+
   def set_default_type
     if !self.channel_type
       self.channel_type = "public"
     end
   end
-  
+
   def subscribe_owner
     if self.channel_type != "group"
       user_subscribers << owner rescue ActiveRecord::RecordInvalid
@@ -108,7 +108,7 @@ class Channel < ActiveRecord::Base
 #    group_subscriber_ids.each do |subscriber|
 #      errors.add("Group #{subscriber} does not exist and cannot be subscribed to channel.") if !Group.exists?(subscriber)
 #    end
-      
+
   end
 
 end
