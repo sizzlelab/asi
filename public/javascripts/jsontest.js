727651239df6474b5bb0542b12ba3643bb6b78b4
Ville Sundberg <sundberg.ville@gmail.com>
2009-08-13 07:40:34 +0000

    #96 HTTPS:n vaatiminen kirjautumisessa implementoitu, ei väännetty päälle

--- public/javascripts/jsontest.js
+++ public/javascripts/jsontest.js
@@ -1,14 +1,21 @@
 http = new XMLHttpRequest();
 
 function test(token, base) {
-    var url = document.getElementById('url').value;
+    var url = "";
+    if (document.domain == "localhost") {
+        url = document.getElementById('url').value;
+    } else {
+        url = "https://" + location.host + document.getElementById('url').value;
+    }
+
     var method = document.getElementById('method').value;
     var params = "authenticity_token=" + token + "&" + document.getElementById('params').value;
+
     http.open(method, url, true);
 
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     http.setRequestHeader("Accept", "application/json, image/*");
-		http.setRequestHeader("Content-length", params.length);
+    http.setRequestHeader("Content-length", params.length);
     http.setRequestHeader("Connection", "close");
 
     http.onreadystatechange = useHttpResponse;
@@ -17,17 +24,17 @@ function test(token, base) {
 
 function useHttpResponse() {
     if (http.readyState == 4) {
-	var resp = document.createElement("div");
-	resp.className = "response";
-	var code = document.createElement("strong");
-	code.appendChild(document.createTextNode("Code: "));
-	resp.appendChild(code);
-	resp.appendChild(document.createTextNode(http.status));
-	resp.appendChild(document.createElement("br"));
-	var body = document.createElement("strong");
-	body.appendChild(document.createTextNode("Body: "));
-	resp.appendChild(body);
-	resp.appendChild(document.createTextNode(http.responseText));
-	document.getElementById('responses').insertBefore(resp, document.getElementById('responses').firstChild);
+        var resp = document.createElement("div");
+        resp.className = "response";
+        var code = document.createElement("strong");
+        code.appendChild(document.createTextNode("Code: "));
+        resp.appendChild(code);
+        resp.appendChild(document.createTextNode(http.status));
+        resp.appendChild(document.createElement("br"));
+        var body = document.createElement("strong");
+        body.appendChild(document.createTextNode("Body: "));
+        resp.appendChild(body);
+        resp.appendChild(document.createTextNode(http.responseText));
+        document.getElementById('responses').insertBefore(resp, document.getElementById('responses').firstChild);
     }
 }
