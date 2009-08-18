06f22682b1824aa4318e5e024fe026c2159f9139
Ville Sundberg <sundberg.ville@gmail.com>
2009-08-18 14:29:58 +0000

    Siirretty serverkohtaiset asetukset

--- lib/recipes/thinking_sphinx.rb
+++ lib/recipes/thinking_sphinx.rb
@@ -1,7 +1,7 @@
 namespace :thinking_sphinx do
   [ :configure, :index, :start, :stop, :restart ].each do |t|
     task t, :roles => :app do
-      run "cd #{current_path}; rake thinking_sphinx:#{t} --trace RAILS_ENV=#{rails_env}"
+      run "cd #{current_path}; rake thinking_sphinx:#{t} RAILS_ENV=#{rails_env}"
     end
   end
 end
