2fe97365aa8d824778bcbe2aedc240f352fce949
Otto Vehviläinen <otto.vehvilainen@iki.fi>
2009-08-24 10:41:26 +0000

    Developmentista cache pois

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,7 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 
-config.action_controller.perform_caching             = true
-config.cache_store = :memory_store, "127.0.0.1:11211"
+config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
