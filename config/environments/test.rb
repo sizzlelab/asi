2c296265f68d5970d7cf4b5dde87dddd8b3cff5c
Otto Vehviläinen <otto.vehvilainen@iki.fi>
2009-08-24 07:33:24 +0000

    MemoryStore developmentiin ja testiin päälle

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,7 +12,7 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = true
-config.cache_store = :mem_cache_store
+config.cache_store = :memory_store
 
 
 # Disable request forgery protection in test environment
