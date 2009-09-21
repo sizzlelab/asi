31ba65fa9559fbedc5ba5f17af5a1b666417aa1d
Sampo Toiva <sampo.toiva@tkk.fi>
2009-09-21 11:42:19 +0000

    Beta.rb conffista muutettu RESSI_URL osoittamaan http://cos.alpha.sizl.org/ressi -osoitteeseen.

--- config/beta.rb
+++ config/beta.rb
@@ -1,4 +1,4 @@
 SERVER_DOMAIN = "http://cos.sizl.org"
-RESSI_URL = "http://cos.sizl.org/ressi"
+RESSI_URL = "http://cos.alpha.sizl.org/ressi"
 CAS_BASE_URL = "https://cos.sizl.org:8443/cas"
 LOG_TO_RESSI = true
