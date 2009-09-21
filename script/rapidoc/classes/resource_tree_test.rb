04bba5b7d6dbadfeac0fcf4aba5ce90183f8ffb0
Sampo Toiva <sampo.toiva@tkk.fi>
2009-09-21 14:01:38 +0000

    Sisäiset viittaukset toimimaan dokumentoinnissa. Jos useampi metodi viittaa samaan controllerin actioniin luodaan sisäiset viittaukset automaattisesti.

--- script/rapidoc/classes/resource_tree_test.rb
+++ script/rapidoc/classes/resource_tree_test.rb
@@ -4,6 +4,6 @@ require 'fileutils'
 require File.join(File.dirname(__FILE__), 'resource_tree.rb')
 require File.join(File.dirname(__FILE__), 'doc_util.rb')
 
-tree = ResourceTree.new ['/', 'api', 'doc', 'admin', 'coreui', 'tutorial', 'test', 'transactions', 'people/change_password', 'people/reset_password']
+tree = ResourceTree.new ['/', 'api', 'doc', 'admin', 'coreui', 'tutorial', 'test', 'transactions', 'people/change_password', 'people/reset_password', 'system', 'confirmation']
 
 puts tree
