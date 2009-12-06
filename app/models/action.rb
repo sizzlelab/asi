ee87e9820bc620d16037c2c83438877a8eb6e045
Ville Sundberg <sundberg.ville@gmail.com>
2009-12-06 09:56:41 +0000

    coreui:n toimiminen lahempana

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,12 +2,14 @@ class Action < ActiveRecord::Base
   has_many :condition_action_sets
   has_many :conditions, :through => :condition_action_sets
 
-  validates_presence_of [:action_type, :action_data]
+  usesguid
+  
+  validates_presence_of [:action_type, :action_value]
 
   def Action.get_or_create(options)
-    action = Action.find(:first, :conditions => { :action_type => options[:action_type], :action_value => options[:action_value] })
-    if action.length == 0
-      action = Action.new(:action_type=> options[:action_type], :action_value => options[:action_value])
+    action = Action.find(:first, :conditions => options)
+    if not action
+      action = Action.new(options)
       if action.save
         return action
       end
