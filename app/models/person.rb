23ca276af6fd8cf9fac066bd1e32615b48b9d4c7
Antti Virolainen <gnomet@gmail.com>
2011-07-29 10:07:35 +0300

    Allow more special characters in email addresses (that should be allowed according to specifications)

--- app/models/person.rb
+++ app/models/person.rb
@@ -157,7 +157,7 @@ class Person < ActiveRecord::Base
                       :allow_nil => true
 
   validates_format_of :email,
-                      :with => /^[A-Z0-9._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i,
+                      :with => /^[A-Z0-9._%\-\+\~\/]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i,
                       :message => "is invalid",
                       :allow_blank => true,
                       :allow_nil => true
