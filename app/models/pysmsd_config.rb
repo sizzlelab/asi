29b55e2bb120d5503f4264f46e09dc6227eddf1c
swiftcoach <knkarithi@gmail.com>
2011-09-16 17:50:07 +0300

    Further validations and unit tests added

--- app/models/pysmsd_config.rb
+++ app/models/pysmsd_config.rb
@@ -1,13 +1,20 @@
 class PysmsdConfig < ActiveRecord::Base
    belongs_to :client, :foreign_key => :client_id  
-   validates :pysmsd_app_name,:pysmsd_app_password,:pysmsd_host,:pysmsd_port,:presence => true
-   validates :pysmsd_enabled,:pysmsd_use_ssl, :inclusion => { :in => [true, false] }
-   validates :pysmsd_port, :numericality => {
-     :greater_than => 0,
-     :less_than => 65536,
-     :only_integer => true
+   validates_presence_of :client_id
+   validates_presence_of :pysmsd_app_name,:pysmsd_app_password,:pysmsd_host,:pysmsd_port, :if => :pysmsd_enabled
+   validates_presence_of :pysmsd_proxy_host,:pysmsd_proxy_port, :if => :pysmsd_use_proxy
+   validates_inclusion_of :pysmsd_enabled,:pysmsd_use_ssl, :in => [true, false] 
    
-   }
+   validates_numericality_of :pysmsd_port, 
+     :greater_than_or_equal_to => 0,
+     :less_than => 65536,
+     :only_integer => true,
+     :if => :pysmsd_enabled
+   
+    validates_numericality_of :pysmsd_proxy_port, :allow_nil => true,
+     :greater_than_or_equal_to => 0,
+     :less_than => 65536,
+     :only_integer => true,
+     :if => :pysmsd_use_proxy
      
-
 end
--- test/unit/pysmsd_config_test.rb
+++ test/unit/pysmsd_config_test.rb
@@ -1,15 +1,73 @@
 require 'test_helper'
 
 class PysmsdConfigTest < ActiveSupport::TestCase
-  test "Valid pysmsd port" do
+  def setup
+     
+    @valid_pysmsd_config = pysmsd_configs(:valid_pysmsd_config)
+    @valid_pysmsd_config_with_proxy = pysmsd_configs(:valid_pysmsd_config_with_proxy)
   end
   
   def test_pysmsd_config_validity
-    assert pysmsd_configs(:valid_pysmsd_config).valid?
+    assert @valid_pysmsd_config.valid?
+    assert @valid_pysmsd_config_with_proxy.valid?
   end
   
+  def test_pysmsd_config_disabled_validity
+    valid_config = pysmsd_configs(:valid_pysmsd_config)
+    valid_config.pysmsd_enabled = 0
+    valid_config.pysmsd_app_name = nil
+    assert valid_config.valid?
+
+    valid_config = pysmsd_configs(:valid_pysmsd_config)
+    valid_config.pysmsd_enabled = 0
+    valid_config.pysmsd_app_password = nil
+    assert valid_config.valid?
+
+    valid_config = pysmsd_configs(:valid_pysmsd_config)
+    valid_config.pysmsd_enabled = 0
+    valid_config.pysmsd_host = nil
+    assert valid_config.valid?
+
+    valid_config = pysmsd_configs(:valid_pysmsd_config)
+    valid_config.pysmsd_enabled = 0
+    valid_config.pysmsd_port = nil
+    assert valid_config.valid?
+  end
+    
   def test_pysmsd_config_invalidity
-    assert pysmsd_configs(:invalid_pysmsd_config).invalid?
+    invalid_config = pysmsd_configs(:valid_pysmsd_config)
+    invalid_config.pysmsd_app_name = nil
+    assert invalid_config.invalid?
+    
+    invalid_config = pysmsd_configs(:valid_pysmsd_config)
+    invalid_config.pysmsd_app_password = nil
+    assert invalid_config.invalid?
+    
+    invalid_config = pysmsd_configs(:valid_pysmsd_config)
+    invalid_config.pysmsd_host = nil
+    assert invalid_config.invalid?
+    
+    invalid_config = pysmsd_configs(:valid_pysmsd_config)
+    invalid_config.pysmsd_port = nil
+    assert invalid_config.invalid?
+    
+    invalid_config = pysmsd_configs(:valid_pysmsd_config)
+    invalid_config.pysmsd_port = -10
+    assert invalid_config.invalid?
+  end
+
+  def test_pysmsd_config_proxy_invalidity
+    invalid_config = pysmsd_configs(:valid_pysmsd_config_with_proxy)
+    invalid_config.pysmsd_proxy_host = nil
+    assert invalid_config.invalid?
+
+    invalid_config = pysmsd_configs(:valid_pysmsd_config_with_proxy)
+    invalid_config.pysmsd_proxy_port = nil
+    assert invalid_config.invalid?
+
+    invalid_config = pysmsd_configs(:valid_pysmsd_config_with_proxy)
+    invalid_config.pysmsd_proxy_port = -1010
+    assert invalid_config.invalid?
   end
   
 end
